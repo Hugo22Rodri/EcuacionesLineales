8bb382d47d54dbd4e62486c3f977890c812a3a36
Hugo22Rodri <hadrianrodriguez5@gmail.com>
2025-05-27 11:07:27 -0500

    cambios en las plantillas

--- MetodosNumericos/admin.py
+++ MetodosNumericos/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import SistemaEcuaciones
-
-@admin.register(SistemaEcuaciones)
-class SistemaEcuacionesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'metodo', 'fecha_calculo')
-    search_fields = ('metodo',)
-    list_filter = ('metodo', 'fecha_calculo')
